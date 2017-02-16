d279a6ae830ed0c46039a3cd494da0d42bbafdb3
aoe3 <aoe3@cornell.edu>
2017-02-16 07:40:37 -0500

    Update Triangle.java

--- a2/src/ray1/surface/Triangle.java
+++ a2/src/ray1/surface/Triangle.java
@@ -61,7 +61,65 @@ public class Triangle extends Surface {
    */
   public boolean intersect(IntersectionRecord outRecord, Ray rayIn) {
     // TODO#A2: fill in this function.
-	return false;
+	Vector3 v0 = owner.getMesh().getPosition(face,0);
+	  Vector3 v1 = owner.getMesh().getPosition(face,1);
+	  Vector3 v2 = owner.getMesh().getPosition(face,2);
+	  
+	  Vector3d origin = rayIn.origin.clone();
+	  double eX = origin.clone().x;
+	  double eY = origin.clone().y;
+	  double eZ = origin.clone().z;
+	  
+	  double xAP = (v0.clone().x) - eX;
+	  double yAP = (v0.clone().y) - eY;
+	  double zAP = (v0.clone().z) - eZ;
+	  
+	  Vector3d direction = rayIn.direction.clone();
+	  
+	  Vector3d abc = new Vector3d(a,b,c);
+	  Vector3d def = new Vector3d(d,e,f);
+	  
+	  Matrix3d bigA = new Matrix3d(abc,def,direction);
+	  Vector3d xMinP = new Vector3d(xAP, yAP, zAP);
+	  
+	  //Might need to flip this equation in order to get correct vector
+	  Vector3d betaGammaT = bigA.clone().invert().mul(xMinP.clone());
+	  
+	  double beta = betaGammaT.clone().x;
+	  double gamma = betaGammaT.clone().y;
+	  double t = betaGammaT.clone().z;
+	  //USED FOR TEXTURES
+	  //double alpha = 1 - beta - gamma;
+	  
+	  
+	  if (beta > 0.0){
+		  if(gamma > 0.0){
+			  if (beta + gamma < 1){
+				  
+				  Vector3d p = origin.clone().add(direction.clone().mul(t));
+				  outRecord.location.set(p);
+				  if (!face.hasNormals()){
+					  outRecord.normal.set(this.norm);
+				  } else {
+					  Vector3 bMinA = v1.clone().sub(v0.clone());
+					  Vector3 cMinA = v2.clone().sub(v0.clone());
+					  Vector3 norm = bMinA.clone().cross(cMinA.clone());
+					  outRecord.normal.set(norm).normalize();
+				  }
+				 //ADD TEXTURES
+				 // outRecord.texCoords.set(0,1);
+				  outRecord.surface = this;
+	              outRecord.t = t;
+				  return true;
+			  } else {
+				  return false;
+			  }
+		  } else {
+			  return false;
+		  }
+	  } else {
+		return false;  
+	  }
   }
 
   /**
